86a1874e27c32aee9f66d2f94caf7c1e3a33b22a
Jorden Whicker <jmwhicke@asu.edu>
2013-09-30 14:19:49 -0700

    Important Classes (PARENT)
    I have organized my files and this will be the base of our project.

---
+++ CSE494ViewController.m
@@ -0,0 +1,29 @@
+//
+//  CSE494ViewController.m
+//  Wheres_Miley
+//
+//  Created by Jorden Whicker on 9/30/13.
+//  Copyright (c) 2013 ASU. All rights reserved.
+//
+
+#import "CSE494ViewController.h"
+
+@interface CSE494ViewController ()
+
+@end
+
+@implementation CSE494ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
