400a875994d2297e7719c55a7b484def18e6c1a8
Jorden Whicker <jmwhicke@asu.edu>
2013-09-30 13:48:00 -0700

    Original Code
    Master program for proposal

---
+++ Wheres_Miley/CSE494RulesViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSE494RulesViewController.h
+//  Wheres_Miley
+//
+//  Created by Jorden Whicker on 9/30/13.
+//  Copyright (c) 2013 ASU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSE494RulesViewController : UIViewController
+
+@end
---
+++ Wheres_Miley/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wheres_Miley
+//
+//  Created by Jorden Whicker on 9/30/13.
+//  Copyright (c) 2013 ASU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CSE494AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CSE494AppDelegate class]));
+    }
+}
